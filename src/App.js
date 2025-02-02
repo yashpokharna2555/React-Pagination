7d7079c06c3aa3579cf32d8703109abd8f9d26ac
Yash Pokharna <yashpokharna2512@gmail.com>
2025-02-02 10:28:13 +0000

    Fetch the API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=10&skip=100");
+    const data = await res.json();
+
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return <div className="App"></div>;
 }
