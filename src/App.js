1c119ddebf8cfb7da5bc1842c8408c95c0fa4b2d
Yash Pokharna <yashpokharna2512@gmail.com>
2025-02-03 21:09:55 +0000

    Added pagination logic

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
   const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=10&skip=100");
+    const res = await fetch("https://dummyjson.com/products?limit=100&skip=100");
     const data = await res.json();
 
     if (data && data.products) {
@@ -17,15 +18,34 @@ export default function App() {
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const selectPageHandler = (pageNo) => {
+    setPage(pageNo)
+  }
   return (
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((item) => {
-            return <span>{item.title}</span>;
+          {products.slice(page*10 - 10,page*10).map((item) => {
+            return (
+              <span className="products__single" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <span>{item.title}</span>
+              </span>
+            )
           })}
         </div>
       )}
+      {
+        products.length > 0 && <div className="pagination">
+          <span>◀️</span>
+          {[...Array(Math.ceil(products.length / 10))].map((_, idx) => {
+            return <span className={page == idx+1 ? "page__selected" : ""} onClick={()=> selectPageHandler(idx+1)} key={idx+1}>{idx+1}</span>;
+          })}
+
+          <span>▶️</span>
+        </div>
+      }
     </div>
   );
 }
