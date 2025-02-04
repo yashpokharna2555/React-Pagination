c0e8112209240b19d5801d81ddef9d081b49c7ee
Yash Pokharna <yashpokharna2512@gmail.com>
2025-02-04 19:10:24 +0000

    started prevpage logic

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default function App() {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=100&skip=100");
+    const res = await fetch(
+      "https://dummyjson.com/products?limit=100&skip=100"
+    );
     const data = await res.json();
 
     if (data && data.products) {
@@ -20,32 +22,44 @@ export default function App() {
   }, []);
 
   const selectPageHandler = (pageNo) => {
-    setPage(pageNo)
-  }
+    setPage(pageNo);
+  };
+
+  const prevPageHandler = (pageNo) => {
+    console.log(pageNo);
+  };
   return (
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.slice(page*10 - 10,page*10).map((item) => {
+          {products.slice(page * 10 - 10, page * 10).map((item) => {
             return (
               <span className="products__single" key={item.id}>
                 <img src={item.thumbnail} alt={item.title} />
                 <span>{item.title}</span>
               </span>
-            )
+            );
           })}
         </div>
       )}
-      {
-        products.length > 0 && <div className="pagination">
-          <span>◀️</span>
+      {products.length > 0 && (
+        <div className="pagination">
+          <span onClick={() => prevPageHandler(page)}>◀️</span>
           {[...Array(Math.ceil(products.length / 10))].map((_, idx) => {
-            return <span className={page == idx+1 ? "page__selected" : ""} onClick={()=> selectPageHandler(idx+1)} key={idx+1}>{idx+1}</span>;
+            return (
+              <span
+                className={page == idx + 1 ? "page__selected" : ""}
+                onClick={() => selectPageHandler(idx + 1)}
+                key={idx + 1}
+              >
+                {idx + 1}
+              </span>
+            );
           })}
 
           <span>▶️</span>
         </div>
-      }
+      )}
     </div>
   );
 }
